2132d585d7cc48499de59518c83ddaa80c5c2f55
Cullan Shewfelt <cullanshewfelt@github.com>
2017-05-30 22:35:56 -0700

    testing

--- server.js
+++ server.js
@@ -40,8 +40,8 @@ function bitCoinAjaxCall() {
         // console.log(parsedBody.BTC);
         BTCprice = parsedBody.BTC;
         request("https://min-api.cryptocompare.com/data/price?fsym=BTC&tsyms=USD,ETH,EUR", function(err, res, body) {
-            var moment_tstamp = moment().format("YYYY-MM-DD+HH:mm:ss").subtract(7, "Hours");
-            console.log(moment_tstamp)
+            var moment_tstamp = moment().subtract(7, "Hours").format("YYYY-MM-DD+HH:mm:ss");
+            // console.log(moment_tstamp)
             var parsedBody = JSON.parse(body);
             console.log(parsedBody.USD);
             USDprice = parsedBody.USD;
